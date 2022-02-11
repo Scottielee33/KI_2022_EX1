3de8b1f987bcce45ad0e3f128dc9283881b6f45b
Scottie Lee <scottie.c.f.lee@gmail.com>
2022-02-11 07:04:43 +0000

    made monte carlo move for white

--- chess.cpp
+++ chess.cpp
@@ -540,19 +540,47 @@ int Chess::playthegame (int maxgamelength, bool print,
 void Chess::MCwhitemove (int maxgamelength, int playouts) {
   // TODO 
   //curr -> childnodes ->
+  int best_move = 0;
+  int amount_of_wins = 0;
 
-  for(int i=0; i<numberofwhitemoves();i++){
+  for (int i=0; i < numberofwhitemoves(); i++) {
+    int temp_win = 0;
     Chess copy = *this;
+    
     copy.dowhitemove(i);
-    for(int j=0; j<playouts;j++){
-      for(int k=0; k<maxgamelength;k++){
-        Chess cc = copy;
-        cc.doblackkingmove(j);
-        cc.randomwhitemove();
-      }
-    }
-  }
-}//Chess::MCwhitemove
+    
+    for(int j=0; j<playouts;j++) {
+      Chess cc = copy;
+      int move = 3;
+      while(cc.countmoves < maxgamelength && move==3) {
+        if (cc.whoistomove) {
+          cc.randomwhitemove();
+        }
+        else {
+          move = cc.blackmove(0);
+        }
+      };
+      if (cc.countmoves == maxgamelength ) {
+        if (cc.queencaptured ) {
+        move = 2;
+        }//if
+      else if (cc.numberofblackmoves ( ) == 0 ) {
+        if (cc.incheck (cc.xBK,cc.yBK) )
+          move = 0;
+        else
+          move = 1;
+        };
+      if (move==0)
+        temp_win++;
+      };
+    };
+    if (temp_win > amount_of_wins){
+      amount_of_wins = temp_win;
+      best_move = i;
+    };
+  };
+  dowhitemove(best_move);
+};//Chess::MCwhitemove
 
 // do one clever move for white
 void Chess::cleverwhitemove ( ) {
